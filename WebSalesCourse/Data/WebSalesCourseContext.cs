02d2528d1d05adfa3975eb482e78bc2002564e7f
Carolina Chaves <carolina.amaralchaves@gmail.com>
2020-06-01 18:49:11 -0300

    Basic Structure

---
+++ WebSalesCourse/Data/WebSalesCourseContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebSalesCourse.Models;
+
+namespace WebSalesCourse.Data
+{
+    public class WebSalesCourseContext : DbContext
+    {
+        public WebSalesCourseContext (DbContextOptions<WebSalesCourseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebSalesCourse.Models.Department> Department { get; set; }
+    }
+}
