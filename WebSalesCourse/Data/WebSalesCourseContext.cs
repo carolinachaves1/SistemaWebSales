d746f3444b1b4f8131bce27a6260d06ba642e13b
Carolina Chaves <carolina.amaralchaves@gmail.com>
2020-06-01 20:28:43 -0300

    Added OtherEntities Migration

--- WebSalesCourse/Data/WebSalesCourseContext.cs
+++ WebSalesCourse/Data/WebSalesCourseContext.cs
@@ -14,6 +14,8 @@ namespace WebSalesCourse.Data
         {
         }
 
-        public DbSet<WebSalesCourse.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
+        public DbSet<Seller> Seller { get; set; }
     }
 }
--- WebSalesCourse/Migrations/20200601232525_OtherEntities.Designer.cs
+++ WebSalesCourse/Migrations/20200601232525_OtherEntities.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebSalesCourse.Data;
 
 namespace WebSalesCourse.Migrations
 {
     [DbContext(typeof(WebSalesCourseContext))]
-    partial class WebSalesCourseContextModelSnapshot : ModelSnapshot
+    [Migration("20200601232525_OtherEntities")]
+    partial class OtherEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
