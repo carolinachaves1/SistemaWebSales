5bf1bc49a3aa2f8d8a9d50d011e0552692d27fe2
Carolina Chaves <carolina.amaralchaves@gmail.com>
2020-06-02 09:49:50 -0300

    Added SellerDetails

--- WebSalesCourse/Services/SellerService.cs
+++ WebSalesCourse/Services/SellerService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -24,6 +25,18 @@ namespace WebSalesCourse.Services
         public void Insert(Seller seller)
         {
             _context.Add(seller);
+            _context.SaveChanges();
+        }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller.Include(x => x.Department).FirstOrDefault(x => x.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var seller = _context.Seller.Find(id);
+            _context.Seller.Remove(seller);
             _context.SaveChanges();
         }
 
