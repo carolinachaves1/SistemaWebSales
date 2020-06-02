09c4a88a334eb26cc33b3cef50489fb6233bba1c
Carolina Chaves <carolina.amaralchaves@gmail.com>
2020-06-01 21:10:48 -0300

    SellerController

---
+++ WebSalesCourse/Services/SellerService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebSalesCourse.Data;
+using WebSalesCourse.Models;
+
+namespace WebSalesCourse.Services
+{
+    public class SellerService
+    {
+        private readonly WebSalesCourseContext _context;
+
+        public SellerService(WebSalesCourseContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
--- WebSalesCourse/Startup.cs
+++ WebSalesCourse/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using WebSalesCourse.Data;
+using WebSalesCourse.Services;
 
 namespace WebSalesCourse
 {
@@ -36,6 +37,7 @@ namespace WebSalesCourse
                     builder.MigrationsAssembly("WebSalesCourse")));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
