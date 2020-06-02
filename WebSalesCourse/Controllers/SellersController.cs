1a84bceac674cb8fbe60be300edbca06a54d819e
Carolina Chaves <carolina.amaralchaves@gmail.com>
2020-06-02 08:47:27 -0300

    Created CreateView Seller

--- WebSalesCourse/Controllers/SellersController.cs
+++ WebSalesCourse/Controllers/SellersController.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
+using WebSalesCourse.Models;
 using WebSalesCourse.Services;
 
 namespace WebSalesCourse.Controllers
@@ -21,6 +18,19 @@ namespace WebSalesCourse.Controllers
             var list = _sellerService.FindAll();
 
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
--- WebSalesCourse/Services/SellerService.cs
+++ WebSalesCourse/Services/SellerService.cs
@@ -21,6 +21,11 @@ namespace WebSalesCourse.Services
             return _context.Seller.ToList();
         }
 
+        public void Insert(Seller seller)
+        {
+            _context.Add(seller);
+            _context.SaveChanges();
+        }
 
     }
 }
